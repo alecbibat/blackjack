8359ffd2dabaf5a5d12dcfc046049478b0ed57e5
Alec Bibat <alecbibat@gmail.com>
2015-05-02 10:58:59 -0600

    everything except ace mod

--- blackjack.rb
+++ blackjack.rb
@@ -8,7 +8,7 @@
 deck = ["2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
 				"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
 				"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
-			  "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"]
+				"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"]
 
 card_values = {"2C" => 2, "3C" => 3, "4C" => 4, "5C" => 5, "6C" => 6, "7C" => 7, "8C" => 8, "9C" => 9, "10C" => 10, "JC" => 10, "QC" => 10, "KC" => 10, "AC" => 11,
 							 "2H" => 2, "3H" => 3, "4H" => 4, "5H" => 5, "6H" => 6, "7H" => 7, "8H" => 8, "9H" => 9, "10H" => 10, "JH" => 10, "QH" => 10, "KH" => 10, "AH" => 11,
@@ -22,7 +22,7 @@ fields = [:name, :suit, :value, :usage]
 #change the rands so the program counts the deck
 #then does rand(0..deck.count - 1)
 def deal(deck, card_values)
-	puts "picking random item..."
+	puts "Hit enter to deal cards."
 	gets.chomp()
 	deck_count = deck.count - 1
 	rand = rand(0..deck_count)
@@ -53,7 +53,7 @@ def deal(deck, card_values)
 	rand_4 = rand(0..deck_count)
 	d_card_2 = deck[rand_4]
 	puts "******************"
-	puts "DEALER'S SECOND CARD : #{deck[rand_4]}" #deck[rand]
+	puts "DEALER'S SECOND CARD : ??" #deck[rand], don't show values to player
 	puts "******************"
 	deck = deck - ["#{deck[rand_4]}"]
 	#puts deck
@@ -71,7 +71,7 @@ def evaluate_hand(deck, card_1, card_2, card_values, d_card_1, d_card_2)
 	
 
 	d_hand_value = card_values[d_card_1] + card_values[d_card_2]
-	puts "The dealer's hand value is #{d_hand_value}"
+	puts "The dealer's hand value is ??"
 
 	hit_or_stay(deck, hand_value, card_1, card_2, card_values, d_hand_value)
 	#we can evaluate the dealer's hand here too\
@@ -84,7 +84,8 @@ end
 #if it isn't, asks user to hit or stay
 #passes to methods 'hit' or 'stay' respectively
 def hit_or_stay(deck, hand_value, card_1, card_2, card_values, d_hand_value)
-	if d_hand_value < 17
+	#hits if the dealer has a hand value under 17
+  if d_hand_value < 17
 		deck_count = deck.count - 1
 		rand_4 = rand(0..deck_count)
 		d_card_next = deck[rand_4]
@@ -95,27 +96,31 @@ def hit_or_stay(deck, hand_value, card_1, card_2, card_values, d_hand_value)
 		deck = deck - ["#{deck[rand_4]}"]
 		d_hand_value = d_hand_value + card_values[d_card_next]
 		puts "The dealer's new hand value is #{d_hand_value}!"
-	else
+	#if dealer's hand value is over 17, dealer stays
+  else
 		puts "The dealer stays!"
 	end
 
-	if d_hand_value >= 21
-		game_over(hand_value, d_hand_value)
-		exit
-	elsif d_hand_value == 21
-		game_over(hand_value, d_hand_value)
-		exit
-	else
-	end
+	##this code is useless
+  #if d_hand_value >= 21
+	#	game_over(hand_value, d_hand_value)
+	#	exit
+	#elsif d_hand_value == 21
+	#	game_over(hand_value, d_hand_value)
+	#	exit
+	#else
+	#end
 
-	if hand_value >= 21
-		game_over(d_hand_value, hand_value)
-		exit
-	elsif d_hand_value >=21
-		game_over(d_hand_value, hand_value)
-		exit
-	else
-	end
+	#if hand_value >= 21
+	#	game_over(d_hand_value, hand_value)
+	#	exit
+	#elsif d_hand_value >=21
+	#	game_over(d_hand_value, hand_value)
+	#	exit
+	#else
+	#end
+
+  gets.chomp()
 
 	puts "Input 1 to hit"
 	puts "Input 2 to stay"
@@ -161,20 +166,20 @@ end
 #makes a statement then ends the program
 def game_over(hand_value, d_hand_value)
 
-	puts "Your hand value was #{d_hand_value}"
-	puts "The dealer's hand value was #{hand_value}"
+	puts "Your hand value was #{hand_value}"
+	puts "The dealer's hand value was #{d_hand_value}"
 	if hand_value > 21 and d_hand_value > 21
 		puts "You both busted, nobody wins!"
-	elsif hand_value > 21 and d_hand_value <= 21
-		puts "The dealer busted, you win!"
 	elsif d_hand_value > 21 and hand_value <= 21
+		puts "The dealer busted, you win!"
+	elsif hand_value > 21 and d_hand_value <= 21
 		puts "You busted, the dealer wins!"
-	elsif d_hand_value > hand_value
-		puts "You were closer to 21, you win!"
 	elsif hand_value > d_hand_value
+		puts "You were closer to 21, you win!"
+	elsif d_hand_value > hand_value
 		puts "The dealer was closer to 21, you lose!"
 	else
-		puts "WHAT THE FUCK JUST HAPPENED"
+		puts "WHAT THE FUCK JUST HAPPENED, I guess it was a tie."
 	end
 end
 
